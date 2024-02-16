17e7dc417a5c3449f8b99d1ecfd5006a3e4bb8d7
ChanDon <idonchan110@gmail.com>
2024-02-16 23:15:34 +0900

    webscraper 3

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import time
 #why use time? because, time import is can be delay and prevent the code. So, that allows to view code sequentially.
 from bs4 import BeautifulSoup
 #bs4 == beautifulSoup4
+import csv
 
 
 p = sync_playwright().start()
@@ -14,39 +15,40 @@ page = browser.new_page()
 
 page.goto("https://www.wanted.co.kr/")
 
-time.sleep(4)
+time.sleep(1)
 # delay 7 seconds.
 
 page.click("button.Aside_searchButton__Xhqq3")
 #click the search button
 
-time.sleep(4)
+time.sleep(1)
 
 page.get_by_placeholder("검색어를 입력해 주세요.").fill("DataEngineer")
 #find the placeholder, .fill ("Search text") / placeholder is can fill the .fill function
 
-time.sleep(4)
+time.sleep(1)
 
 page.keyboard.down("Enter")
 
-time.sleep(10)
+time.sleep(1)
 
 page.click("a#search_tab_position")
 
-time.sleep(5)
+time.sleep(10)
 
-for i in range(5):
-    #do 'i' 5 times. So, under code will be run 5 times.
-    time.sleep(5)
-    page.keyboard.down("PageDown")
-    #End = 1 page down
-    #PageDown = 1 Scroll down
+# for i in range(5):
+#     #do 'i' 5 times. So, under code will be run 5 times.
+#     time.sleep(5)
+#     page.keyboard.down("PageDown")
+#     #End = 1 page down
+#     #PageDown = 1 Scroll down
 
 content = page.content()
 #'print(page.content())' is get html what we see.
 
-# p.stop()
+p.stop()
 # p is the page. so, this code say stop page. kill the page
+# if you want to use csv. you must use 'p.stop()' function 
 
 soup = BeautifulSoup(content, "html.parser")
 #import clean-up html
@@ -88,6 +90,15 @@ for job in jobs:
     }
     jobs_db.append(job)
 
-print(jobs_db)
-print(len(jobs_db))
-# len is get the how many number(jobs)
+file = open("jobs.csv", "w", encoding = "utf-8")
+#file = open("jobs.csv") can open jobs.csv. But you don't have jobs.csv, it also make jobs.cvs
+#And "w" mean is writing mode. "r" is read mode.
+#encoding = "utf-8" means see korean!
+writer = csv.writer(file)
+# if i use writter. the use data go to the jobs.csv
+writer.writerow(["title", "company_name", "location", "reward", "link",])
+
+for job in jobs_db:
+    writer.writerow(job.values())
+    #'.values' function is take values each keys.
+    # if you want key like 'Title', 'Location'. You must use '.keys' function
